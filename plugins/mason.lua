545c6a48a1b8d5276dda3c6f8818cfcac917e92c
Lavabyrd <mark@lavabyrd.space>
2023-10-27 11:22:27 +0100

    adds setup and config

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -7,7 +7,13 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "lua_ls",
+        "lua_ls",
+        "pyright",
+        "dockerls",
+        "gopls",
+        "html",
+        "tflint",
+        "yamlls",
       })
     end,
   },
@@ -18,8 +24,9 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "prettier",
-        -- "stylua",
+        "prettier",
+        "stylua",
+        "terraformls",
       })
     end,
   },
@@ -29,7 +36,7 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "python",
+        "python",
       })
     end,
   },
--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -3,7 +3,15 @@ return {
   opts = function(_, opts)
     -- add more things to the ensure_installed table protecting against community packs modifying it
     opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-      -- "lua"
+      "lua",
+      "diff",
+      "dockerfile",
+      "go",
+      "json",
+      "python",
+      "terraform",
+      "vim",
+      "yaml"
     })
   end,
 }
